9eb9e396199a2d2e2e9402692a6d974157e19f44
Abhijit Kumar <abhijit.kumar@appbrew.tech>
2024-07-23 23:34:28 +0530

    added clear buttons to form items with their functionality, added a message which shows when a log is successfully written to the database, added the functionality of message dissapearing after 1 hour

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { addDoc, collection } from "firebase/firestore";
 
 function Form({ db, forms, addForm, removeForm }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [startTimeStamp, setStartTimeStamp] = useState(null);
+  const [message, setMessage] = useState("");
 
   const initData = function () {
     const now = new Date();
@@ -23,6 +24,7 @@ function Form({ db, forms, addForm, removeForm }) {
     try {
       const newLogRef = await addDoc(collection(db, "daily-log-24"), newLog);
       console.log(`Document written with id ${newLogRef.id}`);
+      setMessage(`Document written with id ${newLogRef.id}`);
     } catch (err) {
       console.log("Error adding document: " + err);
     } finally {
@@ -30,26 +32,57 @@ function Form({ db, forms, addForm, removeForm }) {
     }
   };
 
+  const clearTitle = function () {
+    setTitle("");
+  };
+
+  const clearDescription = function () {
+    setDescription("");
+  };
+
+  useEffect(() => {
+    if (!message) return;
+    const timer = setTimeout(() => {
+      setMessage("");
+    }, 1 * 60 * 60 * 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [message]);
+
   return (
     <>
       <h2>Hello daily-log</h2>
-      <form onSubmit={(event) => submitData(event)}>
-        <div>
-          <label>Title: </label>
+      <form onSubmit={(event) => submitData(event)} className="form">
+        <div className="form-item">
+          <label className="form-item-label">Title: </label>
           <input
+            className="form-item-input"
             value={title}
             onChange={(event) => setTitle(event.target.value)}
           />
+          <button className="form-item-button" onClick={clearTitle}>
+            Clear
+          </button>
         </div>
-        <div>
-          <label>Description: </label>
+        <div className="form-item">
+          <label className="form-item-label">Description: </label>
           <textarea
+            className="form-item-textarea"
             value={description}
             onChange={(event) => setDescription(event.target.value)}
           />
+          <button className="form-item-button" onClick={clearDescription}>
+            Clear
+          </button>
         </div>
-        <div>
-          <button onClick={initData} disabled={startTimeStamp !== null}>
+        <div className="form-item">
+          <button
+            className="form-button"
+            onClick={initData}
+            disabled={startTimeStamp !== null}
+          >
             Init
           </button>
           <button
@@ -59,12 +92,13 @@ function Form({ db, forms, addForm, removeForm }) {
             Done
           </button>
         </div>
-        <div>
+        <div className="form-item">
           <button onClick={addForm}>+</button>
           <button onClick={removeForm} disabled={forms.length === 1}>
             -
           </button>
         </div>
+        <p className="message">{message}</p>
       </form>
     </>
   );
