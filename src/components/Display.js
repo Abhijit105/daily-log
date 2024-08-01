fdd5db08c9ed0e73d348454d911b57060049fed7
Abhijit Kumar <abhijit.kumar@appbrew.tech>
2024-08-01 23:54:47 +0530

    feat: add update functionality

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,6 +2,7 @@ import { collection, getDocs } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "../libs/firebase";
 import { signInWithGoogle, signOutWithGoogle } from "../libs/firebase";
+import UpdateModal from "./UpdateModal";
 
 function Display() {
   const [date, setDate] = useState("");
@@ -9,6 +10,17 @@ function Display() {
   const [displayedLogs, setDisplayedLogs] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoadingLogs, setIsLoadingLogs] = useState(false);
+  const [displayUpdateModal, setDisplayUpdateModal] = useState(false);
+  const [logToBeUpdated, setLogToBeUpdated] = useState(null);
+
+  const openUpdateModal = function (selectedLog) {
+    setDisplayUpdateModal(true);
+    setLogToBeUpdated(selectedLog);
+  };
+
+  const closeUpdateModal = function () {
+    setDisplayUpdateModal(false);
+  };
 
   const yesterday = async function () {
     const now = new Date();
@@ -22,7 +34,10 @@ function Display() {
       setIsLoadingLogs(true);
       const logsCol = collection(db, "daily-log-24");
       const logsSnapshot = await getDocs(logsCol);
-      const logsList = logsSnapshot.docs.map((doc) => doc.data());
+      const logsList = logsSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
       const logsYesterday = logsList.filter(
         (log) =>
           log.startTimeStamp?.toDate().getTime() >= yesterday.getTime() &&
@@ -50,7 +65,10 @@ function Display() {
         setIsLoadingLogs(true);
         const logsCol = collection(db, "daily-log-24");
         const logsSnapshot = await getDocs(logsCol);
-        const logsList = logsSnapshot.docs.map((doc) => doc.data());
+        const logsList = logsSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
         const logsToday = logsList.filter(
           (log) =>
             log.startTimeStamp?.toDate().getTime() >= currentDate.getTime()
@@ -72,7 +90,10 @@ function Display() {
         setIsLoadingLogs(true);
         const logsCol = collection(db, "daily-log-24");
         const logsSnapshot = await getDocs(logsCol);
-        const logsList = logsSnapshot.docs.map((doc) => doc.data());
+        const logsList = logsSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
         const logsEnteredDate = logsList.filter(
           (log) =>
             log.startTimeStamp?.toDate().getTime() >= enteredDate.getTime() &&
@@ -95,7 +116,10 @@ function Display() {
         setIsLoadingLogs(true);
         const logsCol = collection(db, "daily-log-24");
         const logsSnapshot = await getDocs(logsCol);
-        const logsList = logsSnapshot.docs.map((doc) => doc.data());
+        const logsList = logsSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
         const logsEnteredDate = logsList.filter(
           (log) =>
             log.startTimeStamp?.toDate().getTime() >= enteredDate.getTime() &&
@@ -121,68 +145,76 @@ function Display() {
   }, [errorMessage]);
 
   return (
-    <div className="display">
-      <nav className="nav nav-auth">
-        <button onClick={signInWithGoogle}>Google</button>
-        <button onClick={signOutWithGoogle}>Sign Out</button>
-      </nav>
-      <div className="date-area">
-        <div className="date-item">
-          <button onClick={yesterday}>Yesterday...</button>
+    <>
+      <div className="display">
+        <nav className="nav nav-auth">
+          <button onClick={signInWithGoogle}>Google</button>
+          <button onClick={signOutWithGoogle}>Sign Out</button>
+        </nav>
+        <div className="date-area">
+          <div className="date-item">
+            <button onClick={yesterday}>Yesterday...</button>
+          </div>
+          <div className="date-item">
+            <input
+              type="number"
+              min={1}
+              max={31}
+              value={date}
+              onChange={(event) => setDate(event.target.value)}
+            />
+            <label>DD</label>
+          </div>
+          <div className="date-item">
+            <input
+              type="number"
+              min={1}
+              max={12}
+              value={month}
+              onChange={(event) => setMonth(event.target.value)}
+            />
+            <label>MM</label>
+          </div>
+          <div className="date-item">
+            <button onClick={go}>GO</button>
+          </div>
         </div>
-        <div className="date-item">
-          <input
-            type="number"
-            min={1}
-            max={31}
-            value={date}
-            onChange={(event) => setDate(event.target.value)}
-          />
-          <label>DD</label>
-        </div>
-        <div className="date-item">
-          <input
-            type="number"
-            min={1}
-            max={12}
-            value={month}
-            onChange={(event) => setMonth(event.target.value)}
-          />
-          <label>MM</label>
-        </div>
-        <div className="date-item">
-          <button onClick={go}>GO</button>
-        </div>
+        {isLoadingLogs ? (
+          <span className="loader"></span>
+        ) : (
+          <div className="displayed-logs">
+            {displayedLogs
+              .sort(
+                (a, b) =>
+                  a.startTimeStamp.toDate().getTime() -
+                  b.startTimeStamp.toDate().getTime()
+              )
+              .map((log, i) => (
+                <div className="displayed-log" key={i}>
+                  <div>
+                    <h3>{i + 1}. </h3>
+                  </div>
+                  <div>
+                    <h3>{log.title}</h3>
+                    <p>{log.description}</p>
+                    <h5>
+                      Start: {log.startTimeStamp.toDate().toLocaleString()} End:{" "}
+                      {log.endTimeStamp.toDate().toLocaleString()}
+                    </h5>
+                  </div>
+                  <div>
+                    <button onClick={() => openUpdateModal(log)}>Update</button>
+                  </div>
+                </div>
+              ))}
+          </div>
+        )}
+        <p className="message">{errorMessage}</p>
       </div>
-      {isLoadingLogs ? (
-        <span className="loader"></span>
-      ) : (
-        <div className="displayed-logs">
-          {displayedLogs
-            .sort(
-              (a, b) =>
-                a.startTimeStamp.toDate().getTime() -
-                b.startTimeStamp.toDate().getTime()
-            )
-            .map((log, i) => (
-              <div className="displayed-log" key={i}>
-                <div>
-                  <h3>{i + 1}. </h3>
-                </div>
-                <div>
-                  <h3>{log.title}</h3>
-                  <p>{log.description}</p>
-                  <h5>
-                    Start: {log.startTimeStamp.toDate().toLocaleString()} End:{" "}
-                    {log.endTimeStamp.toDate().toLocaleString()}
-                  </h5>
-                </div>
-              </div>
-            ))}
-        </div>
+      {displayUpdateModal && (
+        <UpdateModal onClose={closeUpdateModal} log={logToBeUpdated} />
       )}
-      <p className="message">{errorMessage}</p>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/UpdateModal.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { db } from "../libs/firebase";
+import { doc, updateDoc } from "firebase/firestore";
+
+function UpdateModal({ onClose, log }) {
+  const [title, setTitle] = useState(log.title);
+  const [description, setDescription] = useState(log.description);
+  const [message, setMessage] = useState("");
+
+  const updateData = async function (event, selectedId) {
+    event.preventDefault();
+    try {
+      const docRef = doc(db, "daily-log-24", selectedId);
+      await updateDoc(docRef, {
+        title,
+        description,
+      });
+    } catch (err) {
+      console.log(err.message);
+      setMessage(err.message);
+    } finally {
+      onClose();
+    }
+  };
+
+  return (
+    <>
+      <div className="modal">
+        <form className="form" onSubmit={(event) => updateData(event, log.id)}>
+          <div className="form-item">
+            <label className="form-item-label">Title: </label>
+            <input
+              id="title"
+              className="form-item-input"
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
+            />
+          </div>
+          <div className="form-item">
+            <label className="form-item-label">Description: </label>
+            <textarea
+              id="description"
+              className="form-item-textarea"
+              value={description}
+              onChange={(event) => setDescription(event.target.value)}
+            />
+          </div>
+          <div className="form-item">
+            <button className="form-button" type="submit">
+              Update
+            </button>
+            <button className="form-button" onClick={onClose}>
+              Cancel
+            </button>
+          </div>
+          {!!message && <p className="message">{message}</p>}
+        </form>
+      </div>
+      <div className="overlay" onClick={onClose}></div>
+    </>
+  );
+}
+
+export default UpdateModal;
