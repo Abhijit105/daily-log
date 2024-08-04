3b13bcdf04a5fe611c08ac22739ca8c30d3d8d91
Abhijit Kumar <abhijit.kumar@appbrew.tech>
2024-08-04 12:54:20 +0530

    feat: add highlights

--- src/components/Display.js
+++ src/components/Display.js
@@ -221,31 +221,37 @@ function Display() {
           <span className="loader"></span>
         ) : (
           <div className="displayed-logs">
-            {displayedLogs
-              .sort(
-                (a, b) =>
-                  a.startTimeStamp.toDate().getTime() -
-                  b.startTimeStamp.toDate().getTime()
-              )
-              .map((log, i) => (
-                <div className="displayed-log" key={i}>
-                  <div>
-                    <h3>{i + 1}. </h3>
+            {!!displayedLogs &&
+              displayedLogs.length !== 0 &&
+              displayedLogs
+                .sort(
+                  (a, b) =>
+                    a.startTimeStamp.toDate().getTime() -
+                    b.startTimeStamp.toDate().getTime()
+                )
+                .map((log, i) => (
+                  <div className="displayed-log" key={i}>
+                    <div>
+                      <h3>{i + 1}. </h3>
+                    </div>
+                    <div>
+                      <h3>{log.title}</h3>
+                      <p>{log.description}</p>
+                      <h5>
+                        Start: {log.startTimeStamp.toDate().toLocaleString()}{" "}
+                        End: {log.endTimeStamp.toDate().toLocaleString()}
+                      </h5>
+                    </div>
+                    <div className="action-buttons">
+                      <button onClick={() => openUpdateModal(log)}>
+                        Update
+                      </button>
+                      <button onClick={() => openDeleteModal(log)}>
+                        Delete
+                      </button>
+                    </div>
                   </div>
-                  <div>
-                    <h3>{log.title}</h3>
-                    <p>{log.description}</p>
-                    <h5>
-                      Start: {log.startTimeStamp.toDate().toLocaleString()} End:{" "}
-                      {log.endTimeStamp.toDate().toLocaleString()}
-                    </h5>
-                  </div>
-                  <div className="action-buttons">
-                    <button onClick={() => openUpdateModal(log)}>Update</button>
-                    <button onClick={() => openDeleteModal(log)}>Delete</button>
-                  </div>
-                </div>
-              ))}
+                ))}
           </div>
         )}
         <p className="message">{errorMessage}</p>
